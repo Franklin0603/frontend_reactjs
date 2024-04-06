c67a1aea6bfec0ebcac403929376db6585278e7a
partapparam <paramsingh1961@gmail.com>
2024-04-05 21:17:25 -0700

    updates to the report page

--- frontend/src/pages/Report2.jsx
+++ frontend/src/pages/Report2.jsx
@@ -43,13 +43,6 @@ const columns = [
 
 export default function Report() {
   const [transactions, setTransactions] = useState([])
-  const [sortConfig, setSortConfig] = useState({
-    key: null,
-    direction: "ascending",
-  })
-  const [query, setQuery] = useState("")
-  const [currentPage, setCurrentPage] = useState(1)
-  const [itemsPerPage] = useState(10)
 
   // Fetch Transactions
   const fetchTransactions = async () => {
@@ -61,64 +54,6 @@ export default function Report() {
   useEffect(() => {
     fetchTransactions()
   }, [])
-
-  // Implement Sorting
-
-  const getSortIndicator = (columnName) => {
-    if (sortConfig.key === columnName) {
-      return sortConfig.direction === "ascending"
-        ? "sort-ascending"
-        : "sort-descending"
-    }
-    return ""
-  }
-
-  const sortedTransactions = useMemo(() => {
-    let sortableItems = [...transactions]
-    if (sortConfig !== null) {
-      sortableItems.sort((a, b) => {
-        if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === "ascending" ? -1 : 1
-        }
-        if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === "ascending" ? 1 : -1
-        }
-        return 0
-      })
-    }
-    return sortableItems
-  }, [transactions, sortConfig])
-
-  const requestSort = (key) => {
-    let direction = "ascending"
-    if (sortConfig.key === key && sortConfig.direction === "ascending") {
-      direction = "descending"
-    }
-    setSortConfig({ key, direction })
-  }
-
-  // 3. Implement Searching
-  // Filter the transactions before sorting them based on the search query.
-
-  const filteredTransactions = useMemo(() => {
-    return sortedTransactions.filter(
-      (transaction) =>
-        transaction.description.toLowerCase().includes(query.toLowerCase()) ||
-        transaction.category.toLowerCase().includes(query.toLowerCase())
-    )
-  }, [sortedTransactions, query])
-
-  // Implement Pagination
-  // Calculate the necessary parameters for pagination based on the filtered transactions.
-
-  const pageCount = Math.ceil(filteredTransactions.length / itemsPerPage)
-  const currentItems = useMemo(() => {
-    const start = (currentPage - 1) * itemsPerPage
-    return filteredTransactions.slice(start, start + itemsPerPage)
-  }, [currentPage, itemsPerPage, filteredTransactions])
-
-  // Update the Render Method
-  // Include input fields for searching, clickable headers for sorting, and buttons for pagination.
 
   return (
     <Box sx={{ height: 400, width: "100%" }}>
